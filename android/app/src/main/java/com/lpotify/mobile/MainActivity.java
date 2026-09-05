581388b6c95b598482f3e8b5c19558f924eea563
Talha-T1 <Talha-T1@users.noreply.github.com>
2026-09-06 00:44:59 +0300

    Add Android (Capacitor) mobile app project

---
+++ android/app/src/main/java/com/lpotify/mobile/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lpotify.mobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lpotify.mobile',
+  appName: 'Lpotify',
+  webDir: 'dist'
+};
+
+export default config;
